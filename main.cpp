edd5fb9c64ba074c9ecf9d823c46109aa84530a2
Anaszairi <anaszairi@student.usm.my>
2026-01-05 19:16:58 +0800

    nah_ikwab

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "test.cpp"
+#include "test2.cpp"
 
 using namespace std;
 
 int main()
 {
     display();
+    display2();
 
     return 0;
 }
---
+++ test2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void display2()
+{
+for ( int i = 1; i <= 5; i++ )
+{
+    cout << "Display from test2.cpp: " << i << endl;
+}
+}
