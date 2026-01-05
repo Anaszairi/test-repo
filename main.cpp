b47d56d70b87fbe054cab855ee0559f8804c4407
ikhwanhakimi76 <ikhwanhakimi@student.usm.my>
2026-01-05 16:42:47 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "test.cpp"
+
+using namespace std;
+
+int main()
+{
+    display();
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void display()
+{
+    for(int i=0; i < 10; ++i)
+    {
+        cout << "nigger" << endl;
+    }
+}
